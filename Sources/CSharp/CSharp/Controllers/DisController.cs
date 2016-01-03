9fc33bc0c6500e246514e001efba513324e3cebe
Sébastien Adam <sebastien.adam.webdev@gmail.com>
2016-01-03 19:02:22 +0100

    Sélection destination WebAPI * Pour la liste des plats, il est possible de donner un type   de destination pour formater les données

--- Sources/CSharp/CSharp/Controllers/DisController.cs
+++ Sources/CSharp/CSharp/Controllers/DisController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CSharp.Models;
+using System;
 using System.Collections.Generic;
 using System.Configuration;
 using System.Linq;
@@ -11,17 +12,39 @@ namespace CSharp.Controllers {
   public class DisController : ApiController {
     private IList<string> AllowedType = new List<string> { "Starter", "MainDish", "Dessert" };
     [HttpGet]
-    public IEnumerable<Dish> Get() {
+    public IEnumerable<object> Get() {
       var DishTypeQuery = HttpContext.Current.Request.QueryString["type"];
-      if((DishTypeQuery != null) && AllowedType.Contains(DishTypeQuery)) {
+      var Target = HttpContext.Current.Request.QueryString["target"];
+      IEnumerable<Dish> DishList = null;
+      if(!String.IsNullOrEmpty(DishTypeQuery) && AllowedType.Contains(DishTypeQuery)) {
         var DishTypeId = Int32.Parse(ConfigurationManager.AppSettings["DishType" + DishTypeQuery]);
         using(ProjetWEBEntities context = new ProjetWEBEntities()) {
-          return context.Dish.Where(dish => dish.DishTypeId == DishTypeId).ToArray();
+          DishList = context.Dish.Where(dish => dish.DishTypeId == DishTypeId).ToArray();
         }
       } else {
         using(ProjetWEBEntities context = new ProjetWEBEntities()) {
-          return context.Dish.ToArray();
+          DishList = context.Dish.ToArray();
         }
+      }
+      switch(Target) {
+        case "Select":
+          return (from dish in DishList
+                 select new {
+                   Value = dish.DishId,
+                   Text = (String.IsNullOrEmpty(DishTypeQuery) ? String.Format("{0} ({1})", dish.Name, dish.Type) : dish.Name)
+                 }).ToArray();
+        //case "Grid":
+        //  return (from dish in DishList
+        //          select new DishListItem {
+        //            Name = dish.Name,
+        //            Type = dish.Type,
+        //            Buttons = String.Format("<a class=\"btn btn-default\" href=\"{1}/{0}\">Modifier</a> | <a class=\"btn btn-default\" href=\"{2}/{0}\">Supprimer</a>",
+        //                                    dish.DishId,
+        //                                    Url.Content("~/Dish/Edit"),
+        //                                    Url.Content("~/Dish/Delete")),
+        //          }).ToArray();
+      default:
+          return DishList;
       }
     }
   }
---
+++ Sources/CSharp/CSharp/Models/DishListItem.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace CSharp.Models {
+  public class DishListItem {
+    [Display(Name = "Nom")]
+    public string Name { get; set; }
+    public string Type { get; set; }
+    public string Buttons { get; set; }
+  }
+}
