ed750900d7d29481de2eeba226c7141ba7802f22
Sébastien Adam <sebastien.adam.webdev@gmail.com>
2016-01-03 14:01:19 +0100

    Préparation mes données

---
+++ Sources/CSharp/CSharp/Controllers/MyDataController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CSharp.Controllers {
+  /*[Authorize(Roles = "Manager,Client")]*/
+  public class MyDataController : Controller {
+    // GET: MyData
+    public ActionResult Index() {
+      return View();
+    }
+    public ActionResult Reservation() {
+      return View();
+    }
+    public ActionResult Client() {
+      return View();
+    }
+  }
+}
